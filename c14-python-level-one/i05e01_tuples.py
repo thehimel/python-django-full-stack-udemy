0889db8f0ecb70c325d0b19b45b4dd630b99438d
Himel Das <himelhub@gmail.com>
2020-06-27 16:55:08 +0600

    refactor

--- c14-python-level-one/i05e01_tuples.py
+++ c14-python-level-one/i05e01_tuples.py
@@ -15,3 +15,26 @@ print(data[0])  # 1
 
 print(data[-1])  # 2
 print(data[::-1])  # (2, 'a', 1) <- Reversed the tuple with slicing
+
+
+# Named tuple
+import collections
+
+# An item can be represented as a namedtuple
+Item = collections.namedtuple('Item', ['weight', 'value'])
+
+items = [
+    Item(10, 2), Item(29, 10), Item(5, 7),
+    Item(5, 3), Item(5, 1), Item(24, 12)]
+
+for item in items:
+    print(item)
+
+"""
+Item(weight=10, value=2)
+Item(weight=29, value=10)
+Item(weight=5, value=7)
+Item(weight=5, value=3)
+Item(weight=5, value=1)
+Item(weight=24, value=12)
+"""
