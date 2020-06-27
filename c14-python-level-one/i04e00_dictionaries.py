a81cab2418b3180a40dad16f2600fa5edcd68228
Himel Das <himelhub@gmail.com>
2020-06-27 16:22:48 +0600

    refactor

---
+++ c14-python-level-one/i04e00_dictionaries.py
@@ -0,0 +1,37 @@
+"""
+Important Note:
+Dictionaries are the implementation of hash-map in Python.
+Thus, dictionaries doesn't have any order.
+"""
+
+person = {
+    'name': 'Smith',
+    'age': 22,
+    'profession': 'Student'
+}
+
+print(person)  # {'name': 'Smith', 'age': 22, 'profession': 'Student'}
+print(person['name'].upper())  # SMITH
+
+
+food_menu = {
+    'bfast': ['fruits', 'breads', 'milk'],
+    'lunch': 'as usual',
+    'dinner': {
+        'starter': ['cheese ball', 'paneer kabab'],
+        'main': ['rice', 'curry', 'salad'],
+        'desert': ['sweet', 'ice cream']
+    }
+}
+
+print(food_menu)
+
+# Access an item
+print(food_menu['bfast'])  # ['fruits', 'breads', 'milk']
+print(food_menu['bfast'][0])  # fruits
+print(food_menu['dinner']['main'])  # ['rice', 'curry', 'salad']
+print(food_menu['dinner']['main'][2])  # salad
+
+# Adding an item
+food_menu['snacks'] = ['tea', 'biscuits']
+print(food_menu)
