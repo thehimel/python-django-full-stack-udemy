250b4a56f3be1dfb881dbe65ec6ede35cac22f08
Himel Das <himelhub@gmail.com>
2020-06-27 16:41:18 +0600

    refactor

---
+++ c14-python-level-one/i05e00_booleans.py
@@ -0,0 +1,25 @@
+"""
+True, 1, Value - All are treated as True
+
+False, 0, None - All are treated as False
+"""
+
+a = True
+a = 1
+a = 'Something'
+
+if(a):
+    print(a)
+
+
+b = False
+b = 0
+
+c = None
+# Doesn't run as None is treated as False
+if(c):
+    print('C1', c)
+
+# Runs as None is treated as False
+if(not c):
+    print('C2', c)
