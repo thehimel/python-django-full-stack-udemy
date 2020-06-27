894b2df5c3ac42c6080f01c55d4b23282ac12a2d
Himel Das <himelhub@gmail.com>
2020-06-27 18:53:05 +0600

    refactor

---
+++ c14-python-level-one/i08e00_if_elif_else.py
@@ -0,0 +1,22 @@
+# Conditional Statements
+a = 10
+b = 20
+
+if (a == 10):
+    print("a is 10")
+
+if (a < b):
+    print("a is less than b")
+else:
+    print("a is not less than b")
+
+x = 10
+y = 20
+z = 30
+
+if (y < x):
+    print("y is less than x")
+elif (x < y):
+    print("x is less than y")
+else:
+    print("Arrived at the else part")
