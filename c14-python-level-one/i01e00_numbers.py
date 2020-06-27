f1d3b337d9323e546d628b5cbd52e4821c9cc6fe
Himel Das <himelhub@gmail.com>
2020-06-27 12:29:02 +0600

    refactor

---
+++ c14-python-level-one/i01e00_numbers.py
@@ -0,0 +1,35 @@
+# variable declaration
+n = 5
+m = 10
+
+# addition
+the_sum = n + m
+print(the_sum)
+
+# subtraction
+sub = m - n
+print(sub)
+
+# multiplication
+product = n * m
+print(product)
+
+# division
+quotient = 10/3
+print(quotient)  # 3.3333333333333335
+
+# integer division
+int_quotient = 10//3
+print(int_quotient)  # 3
+
+# power
+power = 4 ** 2
+print(power)  # 16
+
+# square root
+root = 25 ** 0.5
+print(root)  # 5
+
+# square root
+root = 25 ** (1/2)
+print(root)  # 5
