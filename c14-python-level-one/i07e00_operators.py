4d721e9ad30b717b56b8d546181068657c3ff33b
Himel Das <himelhub@gmail.com>
2020-06-27 18:47:54 +0600

    refactor

---
+++ c14-python-level-one/i07e00_operators.py
@@ -0,0 +1,24 @@
+# Comparison Operators
+a = 10
+b = 20
+c = 30
+d = 30
+
+a < b
+b > a
+
+a <= b
+b >= a
+
+c == d
+a != d
+
+print(1 != '1')  # True
+
+
+# Logical Operators
+(a < b) and (b < c)
+
+(a < b) or (b < c)
+
+(a < b) or (b < c) or (b < d)
