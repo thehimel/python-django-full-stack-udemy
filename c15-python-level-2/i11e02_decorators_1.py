061f6813f515c21ae2a5fc5ecb2825fd669a9a6f
Himel Das <himelhub@gmail.com>
2020-06-28 22:44:32 +0600

    refactor

---
+++ c15-python-level-2/i11e02_decorators_1.py
@@ -0,0 +1,33 @@
+"""
+Suppose there is a simple function gift(). Now we need to add some code
+before and/or after the gift() function. The decorator() takes the gift(),
+wrap_gift() adds some codes before and after the gift(), and decorator()
+returns wrap_gift().
+
+So, when we run gift = decorator(gift), gift() has
+been changed and wrapped by the wrap_gift() through the decorator().
+
+Then when we call gift() after the decoration. We get,
+Wrapping the gift().
+This is a gift.
+gift() has been wrapped.
+"""
+
+
+def decorator(gift):
+
+    def wrap_gift():
+        print("Wrapping the gift().")
+        gift()
+        print("gift() has been wrapped.")
+
+    return wrap_gift
+
+
+def gift():
+    print("This is a gift.")
+
+
+gift = decorator(gift)
+
+gift()
---
+++ c15-python-level-2/i11e03_decorators_2.py
@@ -0,0 +1,32 @@
+"""
+gift = decorator(gift) can also be done by adding @decorator before gift().
+It does the same job.
+
+Decorator can be imagine in this way. Imagine a wedding ceremony.
+We have few objects like stage, gate, car etc.
+If we send this objects to the decorator, the decorator will wrap the object
+and decorate it. And they will do it by adding some flowers and other
+additional accessories with the main object.
+
+The python decorator also works like that. It takes the function, with
+a wrapper function it wraps the function and returns the wrapper function
+so that the given function becomes a wrapped function.
+"""
+
+
+def decorator(gift):
+
+    def wrap_gift():
+        print("Wrapping the gift().")
+        gift()
+        print("gift() has been wrapped.")
+
+    return wrap_gift
+
+
+@decorator
+def gift():
+    print("This is a gift.")
+
+
+gift()
