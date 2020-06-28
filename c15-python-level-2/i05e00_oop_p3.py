13fbdf509f299d15d08205a814a4ee72c6925b21
Himel Das <himelhub@gmail.com>
2020-06-28 17:05:17 +0600

    refactor

---
+++ c15-python-level-2/i05e00_oop_p3.py
@@ -0,0 +1,42 @@
+"""
+Inheritance:
+The way of creating a class from another class.
+Derived class is created from the base class.
+
+Derived class has all the characteristics of the base class.
+Method overriding is also allowed.
+"""
+
+
+class Animal():
+    def __init__(self):
+        print("Animal created.")
+
+    def who_are_you(self):
+        print("I am an animal.")
+
+    def eat(self):
+        print("Your anumal is eating.")
+
+
+my_animal = Animal()  # Animal created.
+my_animal.eat()  # Your anumal is eating.
+
+
+# Deriving Dog class from base class Animal.
+class Dog(Animal):
+    def __init__(self):
+        print("Dog created.")
+
+    def bark(self):
+        print("Woof! Woof!")
+
+    # Method overriding
+    def eat(self):
+        print("Your Dog is eating.")
+
+
+my_dog = Dog()  # Dog created.
+my_dog.bark()  # Woof! Woof!
+my_dog.who_are_you()  # I am an Animal.
+my_dog.eat()  # Your Dog is eating.
