084f30a679eb572e2f546b8dda5e973afc8f1a39
Himel Das <himelhub@gmail.com>
2020-06-29 18:34:40 +0600

    refactor

--- c16-django-level-1/first_project/first_app/views.py
+++ c16-django-level-1/first_project/first_app/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello World!")
+    my_dictionary = {
+        "insert_me":  "I am coming from first_app/views.py",
+    }
+
+    return render(request, 'first_app/index.html', context=my_dictionary)
