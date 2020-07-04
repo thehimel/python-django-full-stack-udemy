6d0e4031367d9156fe790e0d0379af910c413924
Himel Das <himelhub@gmail.com>
2020-07-04 21:55:54 +0600

    refactor

--- c20-django-level-5/login_system/login_app/views.py
+++ c20-django-level-5/login_system/login_app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from login_app.forms import UserForm, UserProfileInfoForm
 
+# Imports for authentication
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
 
 def index(request):
     return render(request, 'login_app/index.html')
@@ -59,3 +65,40 @@ def register(request):
 
     # In this way, if user already exists, it renders the form showing the error.
     return render(request, 'login_app/register.html', context=dictionary)
+
+
+def user_login(request):
+    if request.method == 'GET':
+        dictionary = {}
+        return render(request, 'login_app/login.html', dictionary)
+
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+
+            else:
+                return HttpResponse("Account not active.")
+
+        else:
+            print("Incorrect username and password!")
+            print(f"Username: {username} and Password: {password}")
+            return HttpResponse("Invalid login details.")
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+
+# An example view that requries login
+@login_required
+def profile(request):
+    return HttpResponse("Login successful!")
