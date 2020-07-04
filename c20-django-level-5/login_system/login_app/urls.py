b00dcd2dcbc960d025a01971d5abcbd215434a4c
Himel Das <himelhub@gmail.com>
2020-07-04 22:32:13 +0600

    refactor

--- c20-django-level-5/login_system/login_app/urls.py
+++ c20-django-level-5/login_system/login_app/urls.py
@@ -5,8 +5,6 @@ app_name = 'login_app'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^pages', views.pages, name='pages'),
-    url(r'^users', views.users, name='users'),
     url(r'^register/$', views.register, name='register'),
     url(r'^user_login/$', views.user_login, name='user_login'),
 ]
--- c20-django-level-5/login_system/login_app/views.py
+++ c20-django-level-5/login_system/login_app/views.py
@@ -12,19 +12,6 @@ def index(request):
     return render(request, 'login_app/index.html')
 
 
-def pages(request):
-    return render(request, 'login_app/pages.html')
-
-
-# Used to exercise Template Filters
-def users(request):
-    user_details = {
-        'username': 'johnsmith',
-        'age': 20
-    }
-    return render(request, 'login_app/users.html', context=user_details)
-
-
 def register(request):
     registered = False
 
