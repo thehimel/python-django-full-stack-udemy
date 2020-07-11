96bb98f7d1ce4db442070e5c846d7ea6cba0c82b
Himel Das <himelhub@gmail.com>
2020-07-11 17:29:43 +0600

    refactor

--- c23-blog-project/blog_project/blog_app/models.py
+++ c23-blog-project/blog_project/blog_app/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')  # Superuser
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    publish_date = models.DecimalField(blank=True, null=True)
+
+    def publish(self):
+        self.publish_date = timezone.now()
+        self.save()
+
+    def approved_comments(self):
+        return self.comments.filter(approved=True)
+
+    def get_absolute_url(self):
+        return reverse('post_details', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog_app.Post', related_name='comments')
+    author = models.CharField(max_length=40)  # Anyone
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approved = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse('post_list')
+
+    def __str__(self):
+        return self.text
--- c23-blog-project/blog_project/blog_project/settings.py
+++ c23-blog-project/blog_project/blog_project/settings.py
@@ -14,6 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'blog_app/templates/blog_app')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'blog_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = STATIC_DIR
+
+LOGIN_REDIRECT_URL = '/'
