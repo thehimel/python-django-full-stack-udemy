617c7d8cda61ffedef1d7bd7b1750ad3ad59011c
thehimel <thehimelofficial@gmail.com>
2023-04-23 02:51:19 +0200

    Updated i08e01_filter_lambda.py

--- c14-python-level-1/i08e01_filter_lambda.py
+++ c14-python-level-1/i08e01_filter_lambda.py
@@ -4,7 +4,6 @@ filter(function or None, iterable) --> filter object
 
 Return an iterator yielding those items of iterable for which function(item)
 is true. If function is None, return the items that are true.
-
 """
 
 
