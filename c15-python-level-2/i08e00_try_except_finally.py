ae59c58c059dcf822f025b47964e5cf413d3a1be
thehimel <thehimelofficial@gmail.com>
2023-04-23 13:49:13 +0200

    Updated i08e00_try_except_finally.py

--- c15-python-level-2/i08e00_try_except_finally.py
+++ c15-python-level-2/i08e00_try_except_finally.py
@@ -53,3 +53,91 @@ except Exception:
 
 finally:
     print("Finally always runs.")
+
+# ---------------------------------------
+"""
+Author: Himel Das
+
+Output:
+12
+No exception is caught.
+Execution completed.
+"""
+
+try:
+    result = 10 + 2
+    print(result)
+except TypeError:
+    print("Caught TypeError.")
+except NameError:
+    print("Caught NameError.")
+else:
+    print("No exception is caught.")
+finally:
+    print("Execution completed.")
+
+# ---------------------------------------
+"""
+Author: Himel Das
+
+Output:
+Caught TypeError.
+Execution completed.
+"""
+
+try:
+    result = 10 + "2"
+    print(result)
+except TypeError:
+    print("Caught TypeError.")
+except NameError:
+    print("Caught NameError.")
+else:
+    print("No exception is caught.")
+finally:
+    print("Execution completed.")
+
+# ---------------------------------------
+"""
+Author: Himel Das
+
+Output:
+Caught NameError.
+Execution completed.
+"""
+
+try:
+    result = 10 + 2
+    print(result_not_exists)
+except TypeError:
+    print("Caught TypeError.")
+except NameError:
+    print("Caught NameError.")
+else:
+    print("No exception is caught.")
+finally:
+    print("Execution completed.")
+
+# ---------------------------------------
+"""
+Author: Himel Das
+
+Output:
+Caught TypeError.
+Execution completed.
+
+Note: There exists 2 exceptions in the code. However, when the first one is
+caught, the program shows only the first exception.
+"""
+
+try:
+    result = 10 + "2"
+    print(result_not_exists)
+except TypeError:
+    print("Caught TypeError.")
+except NameError:
+    print("Caught NameError.")
+else:
+    print("No exception is caught.")
+finally:
+    print("Execution completed.")
